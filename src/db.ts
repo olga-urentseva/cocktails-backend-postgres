05fd1ef865f2520e5b88088b5be81be59c6a0f71
Olga Urentseva <lelyalelya9666@gmail.com>
2025-04-29 23:10:40 -0700

    fix

--- src/db.ts
+++ src/db.ts
@@ -1,8 +1,24 @@
 import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
 import * as schema from "../drizzle/schema";
+import { readFileSync } from "node:fs";
+import { assert } from "node:console";
 
-const queryClient = postgres(process.env["DATABASE_URL"]!);
+function ensure<T>(value: T): NonNullable<T> {
+  if (typeof value === "undefined" || value === null) {
+    throw new Error("missing value");
+  }
+
+  return value;
+}
+
+const DATABASE_URL =
+  process.env["DATABASE_URL"] ??
+  readFileSync(ensure(process.env["DATABASE_URL_SECRET_PATH"]), "utf-8").trim();
+
+assert(DATABASE_URL);
+
+const queryClient = postgres(DATABASE_URL);
 export const db = drizzle(queryClient, { schema, logger: true });
 
 export type Db = typeof db;
